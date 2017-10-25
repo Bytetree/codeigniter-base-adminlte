1c1c8682dfa149cc008b3d01da5dee2bc7de7fbc
Jon Ziemlich <mail@jon-ziemlich.de>
2017-10-25 02:46:07 +0200

    Changed Structure

--- application/config/config.php
+++ application/config/config.php
@@ -100,7 +100,7 @@ $config['charset'] = 'UTF-8';
 | setting this variable to TRUE (boolean).  See the user guide for details.
 |
 */
-$config['enable_hooks'] = FALSE;
+$config['enable_hooks'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -532,4 +532,6 @@ $config['proxy_ips'] = '';
 | directory.
 |
 */
-$config['modules_locations'] = array(APPPATH . 'modules/');
+$config['modules_locations'] = array(
+    APPPATH.'modules/' => '../modules/',
+);
