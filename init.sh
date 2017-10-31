1194b5b72416816c7ffb881a6f671d1975e6ef38
Jon Ziemlich <mail@jon-ziemlich.de>
2017-10-31 15:11:03 +0100

    SH

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
-sed -i '' "s/\%%projectname%%/meinneuesprojekt/" composer.json bower.json package.json
-sed -i '' "s/\%%projectdescription%%/meinneuesprojekt/" composer.json bower.json package.json
-sed -i '' "s/\%%author%%/meinneuesprojekt/" composer.json bower.json package.json
+echo -n 'Codeigniter Boilerplate - Project Name: '
+read projectname
+echo -n 'Codeigniter Boilerplate - Project Description: '
+read projectdescription
+echo -n 'Codeigniter Boilerplate - Project Author: '
+read projectauthor
+
+sed -i '' "s/\%%projectname%%/$projectname/" composer.json bower.json package.json
+sed -i '' "s/\%%projectdescription%%/$projectdescription/" composer.json bower.json package.json
+sed -i '' "s/\%%author%%/$projectauthor/" composer.json bower.json package.json
+composer update
