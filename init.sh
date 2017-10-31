73883a690e2b573b011f7f0b3795eaa52e2ecb5c
Jon Ziemlich <mail@jon-ziemlich.de>
2017-10-31 15:05:01 +0100

    Init

---
+++ init.sh
@@ -0,0 +1,3 @@
+sed -i '' "s/\%%projectname%%/meinneuesprojekt/" composer.json bower.json package.json
+sed -i '' "s/\%%projectdescription%%/meinneuesprojekt/" composer.json bower.json package.json
+sed -i '' "s/\%%author%%/meinneuesprojekt/" composer.json bower.json package.json
